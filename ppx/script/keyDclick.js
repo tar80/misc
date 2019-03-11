ccfb3cb541794c1c0fe17c83992aba8a9eec3607
tar80 <tar80master@gmail.com>
2019-03-12 07:21:47 +0900

    script

--- ppx/script/keyDclick.js
+++ ppx/script/keyDclick.js
@@ -5,29 +5,28 @@ var vpath = PPx.Extract('%FDC');
 
 switch(PPx.Extract(PPx.WindowIDName)){
 case 'C_X':
+  var ppxid = PPx.Extract("%N");
   PPx.Execute('%Oix *setcust X_win:V=B100000000');
   //PPx.Execute('*topmostwindow %NVA,1');
   break;
 default:
-  var ext = PPx.Extract(".%t");
+  var ppxid = PPx.Extract("%N.");
+  var ext = PPx.Extract(".%t").toLowerCase();
   var arc = [".zip",".7z"];
   var image = [".jpg",".jpeg",".bmp",".png",".gif",".vch",".edg",".afd"];
   var doc = [".txt",".ini",".js",".log",".cfg",".html",".ahk",".md"];
-
+  var type = arc.concat(image, doc);
+  PPx.Execute('%Oi *setcust X_win:V=B000000000');
   PPx.Execute('*linecust NoMask,KV_main:CLOSEEVENT,*execute C,*maskentry');
-  for(var item in arc){
+  for(var item in type){
     if(arc[item] == ext){
       PPx.Execute('%K"@C_DIR');
       PPx.Quit(1)
       }
-    }
-  for(var item in image){
     if(image[item] == ext){
-      PPx.Execute('%Oi *setcust X_win:V=B000000000 %:*setcust XV_imgD:VY=-2,4');
+      PPx.Execute('*setcust XV_imgD:VY=-2,4');
       PPx.Execute('*maskentry ' + image + ',!p:*');
     }
-  }
-  for(var item in doc){
     if(doc[item] == ext){
       PPx.Execute('%Oi *setcust X_win:V=B000000000');
       PPx.Execute('*maskentry ' + doc + ',!p:*');
@@ -35,7 +34,7 @@ default:
   }
   break;
 }
-PPx.Execute('*ppv -r -checkeredpattern:on -bootid:A '+ vpath + ' -k *fitwindow %N.');
+PPx.Execute('*ppv -r -checkeredpattern:on -bootid:A '+ vpath + ' -k *fitwindow ' + ppxid);
 
 
 /* else{

79c28c95a9dcde0ea3bc7648205da4fc763ecb63
tar80 <tar80master@gmail.com>
2019-09-30 18:00:57 +0900

    タイトルバーの名称変更

--- ppx/script/changeBranch.js
+++ ppx/script/changeBranch.js
@@ -6,6 +6,6 @@ PPx.Execute('termppx');
 // git branchの状態をテキストに書き出す
 PPx.Execute('%Oni git branch | sed -e s/\' \'*// > %\'temp\'%\\GITBRANCH.TXT');
 // 一行編集で選択したブランチに移動して、PPxのステータス行に反映する
-PPx.Execute('git checkout %*input(-title:"checkout branch" -mode:e -k *completelist /file:%\'temp\'%\\GITBRANCH.TXT) %: git rev-parse --abbrev-ref HEAD | xargs %0pptrayw -c *setcust _user:u_git_branch=');
+PPx.Execute('git checkout %*input(-title:"git checkout" -mode:e -k *completelist /file:%\'temp\'%\\GITBRANCH.TXT) %: git rev-parse --abbrev-ref HEAD | xargs %0pptrayw -c *setcust _user:u_git_branch=');
 PPx.Execute('*wait 400, 1');
 PPx.Execute('%K"@F5');

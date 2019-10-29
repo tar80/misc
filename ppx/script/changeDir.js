4afe535ea595e6fb021901acb6ef5f8e445f0ff0
tar80 <tar80master@gmail.com>
2019-10-30 06:44:59 +0900

    '仮想ディレクトリに簡易対応'

--- ppx/script/changeDir.js
+++ ppx/script/changeDir.js
@@ -1,44 +1,69 @@
 ﻿//!*script
 /* 同階層の隣合うディレクトリに移動 */
+/* 同階層の同じ拡張子の仮想ディレクトリに移動 */
 // PPx.Arguments(0)=1:preview
 // 参照元:http://hoehoetukasa.blogspot.com/2014/01/ppx_29.html
 
-// 親ディレクトリの実態がなければ中止
-PPx.DirectoryType == 1 || PPx.Quit(1);
-
 var fs = PPx.CreateObject('Scripting.FileSystemObject');
-var cDir = PPx.Extract('%FDN');
-var currentDir = fs.GetFolder(cDir);
-// 親ディレクトリがルートorサブディレクトリがなければ終了
-if (currentDir.IsRootFolder || currentDir.ParentFolder.SubFolders.count == 1) {
+var path = PPx.Extract('%FDVN');
+var cDir = path.replace(/^(.*)\\.*\.(?!$).*/, '$1');
+var fileName = path.replace(/^(.*\\)(?!$)(.*)/, '$2');
+var list = [];
+switch (PPx.Extract(PPx.DirectoryType)) {
+  case '1':
+    var currentDir = fs.GetFolder(cDir);
+    var parentDir = currentDir.ParentFolder
+    // 親ディレクトリがルートorサブディレクトリがなければ終了
+    if (currentDir.IsRootFolder || parentDir.SubFolders.count == 1) {
   PPx.SetPopLineMessage('!"サブディレクトリがありません');
-  PPx.Quit(1);
+      PPx.Quit();
+    };
+    var e = new Enumerator(parentDir.SubFolders);
+    /* 属性を考慮してリストに追加 */
+    var get_list = function() {
+      var target = fs.GetFolder(fs.BuildPath(parentDir.Path, e.item().Name));
+      if (target.Attributes <= 17)
+        list.push(e.item().Name);
+    };
+    PPx.Arguments.length ? change_path(-1, 1, 'top') : change_path(1, -1, 'bottom');
+    break;
+  case '4':
+  case '63':
+  case '64':
+  case '96':
+    var parentDir = fs.GetFolder(cDir);
+    var ext = path.replace(/^(.*\.)(?!$)(.*)/, '$2').toLowerCase();
+    var e = new Enumerator(parentDir.Files);
+    /* 拡張子を考慮してリストに追加 */
+    var get_list = function() {
+      var target = fs.GetExtensionName(fs.BuildPath(parentDir.Path, e.item().Name)).toLowerCase();
+      if (target == ext)
+        list.push(e.item().Name);
+    };
+    PPx.Arguments.length ? change_path(-1, 1, 'top') : change_path(1, -1, 'bottom');
+    break;
+  default:
+      PPx.SetPopLineMessage('!"非対応ディレクトリ');
+    break;
 };
-PPx.Arguments.length ? change_dir(-1, 1, 'top') : change_dir(1, -1, 'bottom');
-
-/* ディレクトリの移動を実行する関数 */
-function change_dir(valA, valB, termMessage) {
-  // 同階層のディレクトリのリストを取得
-  var subDir = new Enumerator(currentDir.ParentFolder.SubFolders);
-  var list = new Array();
-  for (subDir.moveFirst(); !subDir.atEnd(); subDir.moveNext()) {
-    //ディレクトリ属性を考慮してリストに追加
-    var subDirName = fs.GetFolder(fs.BuildPath(currentDir.ParentFolder.Path, subDir.item().Name));
-    if (subDirName.Attributes <= 17)
-      list.push(subDir.item().Name);
+/* パス移動を実行する関数 */
+function change_path(valA, valB, termMessage) {
+  // 親ディレクトリからリストを取得
+  for (e.moveFirst(); !e.atEnd(); e.moveNext()) {
+    get_list();
   };
-  // 名前順でソート
+  // リストを名前順でソート
   list.sort(function (a, b) {
     return (a.toLowerCase() < b.toLowerCase() ? valA : valB);
   });
   for (var item in list) {
-    if (list[item] == currentDir.Name)
+    if (list[item] == fileName)
       break;
   };
-  // 対象ディレクトリを取得
-  var tDir = list[Math.max(item - 1, 0)|0];
-  // 対象がなければメッセージを表示
+  // 対象エントリ名を取得
+  var tEntry = list[Math.max(item - 1, 0)|0];
+  // 端ならメッセージを表示
   if (list[item - 2] == null)
     PPx.SetPopLineMessage('!">>' + termMessage);
-  PPx.Execute('*jumppath "' + fs.BuildPath(fs.GetParentFolderName(cDir), tDir) + '"');
+  list[item - 1] == null ? PPx.Quit() : PPx.Execute('*jumppath "' + fs.BuildPath(parentDir.Path, tEntry) + '"');
 };

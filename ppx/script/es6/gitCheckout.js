d9c53ddfe43843957a93f727a220ada888371f53
tar80 <tar80master@gmail.com>
2020-04-19 02:14:57 +0900

    '調整'

--- ppx/script/es6/gitCheckout.js
+++ ppx/script/es6/gitCheckout.js
@@ -14,6 +14,6 @@ const branchList = (()=> {
 PPx.Execute(`%Os *execute C,@git branch | sed -e s/' '*// > ${branchList}`);
 PPx.Execute('%Os *wait 200,1');
 // 一行編集で選択したブランチに移動して、PPxのステータス行に反映する
-PPx.Execute(`%Os *execute C,git checkout %*input(-title:"checkout branch" -mode:e -k *completelist -file:${branchList}) %: *wait 200,2 %: *execute C,*CHECKBRANCH`);
+PPx.Execute(`%Os *execute C,git checkout %*input(-title:"checkout branch" -mode:e -k *completelist -file:${branchList}) %: *wait 200,1 %: *execute C,*CHECKBRANCH`);
 PPx.Execute('%Os *wait 200, 2');
 PPx.Execute('%Os *execute C,%%K"@F5');

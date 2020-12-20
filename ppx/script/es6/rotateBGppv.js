498d4b53b367bd53340f2f62c189d995bce59742
tar80 <tar80master@gmail.com>
2020-12-20 22:18:41 +0900

    [refactor]

--- ppx/script/es6/rotateBGppv.js
+++ ppx/script/es6/rotateBGppv.js
@@ -2,16 +2,7 @@
 /* PPvに背景画像を設定 */
 'use strict';
 const tID = PPx.Extract('%n');
-let type = PPx.Extract(`%*getcust(X_bg:T_${tID})`);
-let path;
-if (type == 0) {
-  [type, path] = [1, PPx.Extract('%FDC')];
-  PPx.Execute('*sound C:\\Windows\\Media\\speech on.wav');
-} else {
-  [type, path] = [0, ''];
-  PPx.Execute('*sound C:\\Windows\\Media\\windows Information bar.wav');
-}
-PPx.Execute(
-  `*setcust X_bg:P_${tID}=${path}
-   *setcust X_bg:T_${tID}=${type}`
-);
+PPx.Execute(`*RotateExecute u_rotate_PPvBG, "*setcust X_bg:P_${tID}="" "" %%: *setcust X_bg:T_${tID}=0 %%:\
+             *sound C:\\Windows\\Media\\windows Information bar.wav"\
+           ,"*setcust X_bg:P_${tID}=""%FDC"" %%: *setcust X_bg:T_${tID}=1 %%:\
+             *sound C:\\Windows\\Media\\speech on.wav"`);

f654456e7532412419fe900e43c8799fc4a7bf23
tar80 <tar80master@gmail.com>
2020-12-28 21:37:38 +0900

    [add]

---
+++ ppx/script/js/setThumbCol.js
@@ -0,0 +1,21 @@
+﻿//!*script
+/* 一括サムネイル設定 */
+// PPx.Arguments() = (0)表示形式(MC_celS), (1)サムネイル候補画像リストパス
+
+var arg = function () {
+  try {
+    return [ PPx.Arguments(0), PPx.Arguments(1) ];
+  } catch(e) {
+    PPx.Echo(e);
+    PPx.Quit(1);
+  }
+}();
+var pictPath = PPx.Extract('%*input(-title:"サムネイル画像の選択" -mode:e -k *completelist -file:"' + arg[1] + '")');
+PPx.Execute('%Oi *viewstyle -temp "' + arg[0] + '" %: *wait 100');
+PPx.EntryFirstMark;
+var l = PPx.EntryMarkCount;
+for (let i = 0; i < l; i++) {
+  PPx.Execute('*setentryimage ' + pictPath + ' -save');
+  PPx.EntryNextMark;
+}
+

8d5f9769a6a108c42d9a4b236c91acb0962060d7
tar80 <tar80master@gmail.com>
2021-01-18 12:44:26 +0900

    [update]ver17605

--- ppx/script/js/openLF.js
+++ ppx/script/js/openLF.js
@@ -14,16 +14,16 @@ if (!PPx.Arguments.length) {
 var arg = [PPx.Arguments(0), (PPx.Arguments.length != 2) ? 0 : PPx.Arguments(1)|0];
 var rep = [];
 
-var waitTime = (arg[1] == 1) ? 0 : 300;
-
 function Exe_edit (path, line, duplicate) {
   switch(arg[0]) {
   case 'gvim':
     rep[0] = PPx.Extract('%hs0').replace(/\\/g, '');
     PPx.Execute('%Oi gvim --remote-tab-silent +"' + line + '-1 /%hs0/ " "' + path + '"');
+    PPx.Sleep(300);
     break;
   case 'ppv':
     PPx.Execute('%Oi *ppv -bootid:C ' + path);
+    PPx.Sleep(100);
     break;
   case 'sed':
     if (typeof rep[0] == 'undefined') {
@@ -39,49 +39,37 @@ function Exe_edit (path, line, duplicate) {
   }
 }
 
+var fso = PPx.CreateObject('Scripting.FileSystemObject');
+
 var markEntry = PPx.Extract('%#FDC').split(' ');
 var markCount = PPx.EntryMarkCount;
-var ObjEntry = PPx.Entry;
+// マークの有無でループの初期値を設定
+var n = (markCount != 0) ? 1 : 0;
 
-var entryInfo = new Decode_entry();
+var exist = {};
+var ObjEntry = PPx.Entry;
 
 PPx.Entry.Index = ObjEntry.FirstMark;
 
-for (var i = 0, l = entryInfo.length; i < l; i++) {
-  // 同一パスを判別してエディタを開く
-  if (arg[1] == 1 || !entryInfo[i].dup) {
-    Exe_edit(entryInfo[i].path, entryInfo[i].line, entryInfo[i].dup);
-    PPx.Sleep(waitTime);
+for (var i = n; i <= markCount; i++) {
+  // 空白行の判定
+  if (fso.FileExists(ObjEntry.Name)) {
+    var entryPath = markEntry[i - n];
+
+    // リストファイルのshortname項目を該当の行番号と見立てる
+    var entryLine = (ObjEntry.ShortName.slice(0, 1).match(/[0-9]/) != null) ? ObjEntry.ShortName : 1;
+
+    // 重複パスの判別
+    var entryDup = function (isDup) {
+      isDup = (exist[entryPath]) ? true : false;
+      exist[entryPath] = true;
+      return isDup;
+    }();
+
+    // 同一パスを判別してエディタを開く
+    if (arg[1] == 1 || !entryDup) {
+      Exe_edit(entryPath, entryLine, entryDup);
+    }
   }
   ObjEntry.NextMark;
 }
-
-/* リストファイルの行情報からオブジェクトを生成する関数 */
-function Decode_entry () {
-  var fso = PPx.CreateObject('Scripting.FileSystemObject');
-  var info = [];
-
-  // マークの有無でループの初期値を設定
-  var n = (markCount != 0) ? 1 : 0;
-  var exist = {};
-
-  PPx.Entry.Index = ObjEntry.FirstMark;
-
-  for (var i = n; i <= markCount; i++) {
-    // 空白行の判定
-    if (fso.FileExists(ObjEntry.Name)) {
-      var en = markEntry[i - n];
-      // リストファイルのshortname項目を該当の行番号と見立てる
-      var sn = (ObjEntry.ShortName.slice(0, 1).match(/[0-9]/) != null) ? ObjEntry.ShortName : 1;
-      var d = function (isDup) {
-        isDup = (exist[ObjEntry.Name]) ? true : false;
-        exist[ObjEntry.Name] = true;
-        return isDup;
-      }();
-      info.push({path: en, line: sn, dup: d});
-    }
-    ObjEntry.NextMark;
-  }
-  return info;
-}
-

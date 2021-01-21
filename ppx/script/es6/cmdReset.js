d9158801ea18944c878a64dc4ec868eb16cd2560
tar80 <tar80master@gmail.com>
2021-01-21 21:22:04 +0900

    [update]add reset

---
+++ ppx/script/es6/cmdReset.js
@@ -0,0 +1,20 @@
+﻿//!*script
+/* 初期化後再設定 */
+
+'use strict';
+
+const pDir = PPx.Extract('%FD');
+const cfgPath = [];
+const l = PPx.EntryMarkCount;
+const ObjEntry = PPx.Entry;
+
+PPx.Entry.Index = ObjEntry.FirstMark;
+
+for (let i = 1; i <= l; i++) {
+  cfgPath.push(`*wait 90,1 %%: PPCUSTW CA ${ObjEntry.Name}`);
+  ObjEntry.NextMark;
+}
+
+PPx.Execute('PPCUSTW CINIT');
+PPx.Execute(`*setcust _Command:reset=*cd ${pDir} %%: ${cfgPath.join(' %%: ')}`);
+PPx.Execute('*reset');

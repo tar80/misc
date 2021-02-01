f14a71a7c50fc83cca7256f5be948cd1cc9cf2da
tar80 <tar80master@gmail.com>
2021-02-01 16:54:52 +0900

    [update]

--- ppx/script/es6/toggleMarkHighlight.js
+++ ppx/script/es6/toggleMarkHighlight.js
@@ -1,19 +1,17 @@
 ﻿//!*script
 /* マークとハイライトをトグル */
 //
-// PPx.Arguments() = (0)Highlight number
 // 参照元:http://hoehoetukasa.blogspot.com/2015/08/blog-post.html
 
 'use strict';
 
-const n = PPx.Arguments.length ? PPx.Arguments(0)|0 : 3;
 const ObjEntry = PPx.Entry;
 
 for (let [i, l] = [0, PPx.EntryDisplayCount]; i < l; i++) {
   if (ObjEntry(i).Mark == 1) {
-    [ObjEntry(i).Highlight, ObjEntry(i).Mark] = [n, 0];
-  } else if (ObjEntry(i).Highlight == n) {
-    [ObjEntry(i).Highlight, ObjEntry(i).Mark] = [0, 1];
+    ObjEntry(i).Mark = 0;
+  } else if (ObjEntry(i).Highlight) {
+    ObjEntry(i).Mark = 1;
   }
 }
 

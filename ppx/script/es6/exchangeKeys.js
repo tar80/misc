a99cfd5d3eb3357ccad02678dc1684397e0ac231
tar80 <tar80master@gmail.com>
2021-03-06 04:31:18 +0900

    Update add script exchangeKeys

--- ppx/script/es6/exchangeKeys.js
+++ ppx/script/es6/exchangeKeys.js
@@ -1,36 +1,39 @@
 ﻿//!*script
-/* キーバインドの一時保存と復元 */
+/* キーバインドの一時保存・設定と復元 */
 //
 // PPx.Arguments(0) = (1):キー保存 | (0):キー復元
-// PPx.Arguments(1) = 保存用項目(M_xxx)のxxx部分
-// PPx.Arguments(2) = 対象とするキー設定ファイルのパス
+// PPx.Arguments(1) = 対象とするキー設定ファイルのパス
 //
-// 登録ex) exchangeKeys.js,1,ck,path\customkeys.cfg
-// 解除ex) exchangeKeys.js,0,ck,path\customkeys.cfg
+// 登録ex) exchangeKeys.js,1,path\customkeys.cfg
+// 解除ex) exchangeKeys.js,0,path\customkeys.cfg
 //
-// ※キー設定ファイルはUTF-8を対象としています。他の文字コードは読めません
-// ※キー設定ファイルには、必ずKC_mainなどの項目名が必要で、
-// 【 項目名  スペース1つ  =  スペース1つ  { 】 の形式でないと失敗します。コメントは付けてもOK
-// ex)
-// XC_main = { ;Comment
-// A , cmd
-// B = @B
-// }
-// ※LEFT = @LEFT のような'='を使う書式は、LEFT , %K"@LEFTに変換されます
-// ※キーバインド中のAltキー、Ctrlキー、Shiftキーは正しい順番(&^\)で書く必要があります
-// ex) ○: &^\A, &^A, &\A
-//     ×: ^\&A, ^&A, \&A
+// ※PPx内部に【M_設定ファイル名】のメニュー項目が一時保存されます
+//
+// キー設定ファイル作成時の注意点
+// 1.UTF-8で保存してください。他の文字コードは読めません
+// 2.対象にできる項目は変数keybinds内で示された項目のみです。また、
+// 【 項目名  スペース  =  スペース  { 】 の形式でないと失敗します。コメントは付けてもOK
+//    ex) KC_main = {
+//        KV_main = { ;comment
+// 3.キーバインドのAltキー、Ctrlキー、Shiftキーは&^\の順番で書く必要があります
+//    ex) ○: &^\A, &^A, ^\A
+//        ×: ^\&A, ^&A, \^A
+// 4.CTRL+0やCTRL+Jなど特定のキーはPPx内部で^V_Hxxの型に変換されて記憶されているようです
+//   CTRL+0の場合、^V_H30としないと復元に失敗します
+// 5.'"'キーと'%'キーはエラーが出るのでいまのところ対象外です
 
 'use strict';
-
-const process = PPx.Arguments(0)|0;
-const title = PPx.Arguments(1);
-const tPath = PPx.Arguments(2);
 
 if (PPx.Arguments.length < 2) {
   PPx.Echo('引数が足りません');
   PPx.Quit(-1);
 }
+
+const process = PPx.Arguments(0)|0;
+const tPath = PPx.Arguments(1);
+const title = PPx.Extract(`%*name(X,${tPath})`);
+const checkDup = PPx.Extract(`%*getcust(M_${title})`).split('\n').length;
+const keybinds = ['KC_main', 'KC_incs', 'K_edit', 'K_ppe', 'K_lied', 'K_tree', 'KB_edit', 'KV_main', 'KV_page', 'KV_crt', 'KV_img'];
 
 const st = PPx.CreateObject('ADODB.stream');
 st.Type = 2;
@@ -44,8 +47,12 @@ const getKeys = [];
 let header;
 
 for (const value of stCnts) {
-  if (value.search(/^[^\s]*\s=\s{.*/) == 0) {
+  if (value.search(/^[^\s]*\s=\s{.*/) === 0) {
     header = value.replace(/^([^\s]*)\s.*/, '$1');
+    if (keybinds.indexOf(header) === -1) {
+      PPx.Echo(`${header}のキー登録は許可されていません`);
+      PPx.Quit(-1);
+    }
   } else if (!value.search(/^[^\s]*\s*[=,].*/)) {
     getKeys.push([header, value.replace(/^([^\s]*)\s.*/, (match, p1) => {
       return p1.replace(/\\'/g, '\\\'');
@@ -56,34 +63,50 @@ for (const value of stCnts) {
 
 if (getKeys[0][0] === undefined) {
   PPx.Echo('項目名が設定されていません');
-  PPx.Quit(1);
+  PPx.Quit(-1);
 }
 
 if (process) {
-  for (const value of getKeys) {
-    const contents = PPx.Extract(`%OC %*getcust(${value[0]}:${value[1]})`);
-    const avoidCnts = contents.replace(/%/g, '%%%%');
+  if (checkDup > 3) {
+    PPx.SetPopLineMessage(`${title}は適用済みです`);
+    PPx.Quit(-1);
+  }
 
-    if (contents == '') {
-      PPx.Execute(`*setcust M_${title}:${value[0]}:${value[1]}=%%%%mByExchangeKeys %%%%K"@${value[1]}`);
+  for (const value of getKeys) {
+    const cnts = PPx.Extract(`%OC %*getcust(${value[0]}:${value[1]})`);
+    if (cnts === '') {
+      PPx.Execute(`*setcust M_${title}:${value[0]}:${value[1]}=%%mNotExist %%K"@${value[1]}`);
     } else {
-      const is = (contents.slice(0,1) == '@' || contents.match(/^[a-zA-Z]*$/)) ? '%%%%K"' : '';
-      PPx.Execute(`%OC *setcust M_${title}:${value[0]}:${value[1]}=${is}${avoidCnts}`);
+      if (cnts.slice(0,1) === '@' || cnts.match(/^[a-zA-Z]*$/)) {
+        PPx.Execute(`*setcust M_${title}:${value[0]}:${value[1]}=%%mSepEQ %%K"${cnts}`);
+      } else {
+        const escCnts = cnts.replace(/%/g, '%%');
+        PPx.Execute(`%OC *setcust M_${title}:${value[0]}:${value[1]}=${escCnts}`);
+      }
     }
   }
+  PPx.Execute(`*setcust @${tPath}`);
 } else {
+  if (checkDup <= 3) {
+    PPx.SetPopLineMessage(`${title}は登録されていません`);
+    PPx.Quit(-1);
+  }
+
   const emptykeys = [];
   for (const value of getKeys) {
-    const exe = PPx.Extract(`%%OC%OC %*getcust(M_${title}:${value[0]}:${value[1]})`);
-    if (exe.indexOf('ByExchangeKeys') != -1) {
+    const cnts = PPx.Extract(`%OC %*getcust(M_${title}:${value[0]}:${value[1]})`);
+    if (cnts === '') {
+      emptykeys.push(value);
+    } else if (cnts.indexOf('mNotExist') !== -1) {
       PPx.execute(`*deletecust ${value[0]}:${value[1]}`);
+    } else if (cnts.indexOf('mSepEQ') !== -1) {
+      PPx.Execute(`*setcust ${value[0]}:${value[1]}=${cnts.replace('%K"', '')}`);
     } else {
-      (exe != '') ?
-        PPx.Execute(`*setcust ${value[0]}:${value[1]},${exe}`):
-        emptykeys.push(value);
+      const escCnts = cnts.replace(/%/g, '%%');
+      PPx.Execute(`%OC *setcust ${value[0]}:${value[1]},${escCnts}`);
     }
   }
-  if (emptykeys != '') { PPx.SetPopLineMessage(`未登録キー: ${emptykeys.join(',')}`); }
+  if (emptykeys.length !== 0) { PPx.SetPopLineMessage(`未登録キー: ${emptykeys.join(',')}`); }
   PPx.Execute(`*deletecust "M_${title}"`);
   PPx.Execute('%K"@LOADCUST');
 }

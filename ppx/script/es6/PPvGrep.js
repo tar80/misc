079a5e919351de801ff633b7b5d88ddb36776104
tar80 <tar80master@gmail.com>
2021-03-09 23:51:35 +0900

    Upadate grepコマンドを新規作成

--- ppx/script/es6/PPvGrep.js
+++ ppx/script/es6/PPvGrep.js
@@ -20,12 +20,13 @@ const use_ripgrep = true;
 const use_jvgrep = true;
 
 // grep_option lockは固定。addの値を変更
+// ※addの記述はハイフン(-)で始まる場合スペースを１つ入れる
 // grep
 const gr = (use_grep === true) ? { 'lock': '-nH', 'add': 'irEC1 --color=always', 'complist': 'GREPOPTION.TXT' } : null;
 // ripgrep
-const rg = (use_ripgrep === true) ? { 'lock': '-n --no-heading ', 'add': '--color always -Li -C1', 'complist': 'RGOPTION.TXT' } : null;
+const rg = (use_ripgrep === true) ? { 'lock': '-n --no-heading', 'add': ' --color always -Li -C1', 'complist': 'RGOPTION.TXT' } : null;
 // jvgrep
-const jv = (use_jvgrep === true) ? { 'lock': '-n -r ', 'add': '--color=always -iGI8 -B1', 'complist': 'JVGREPOPTION.TXT' } : null;
+const jv = (use_jvgrep === true) ? { 'lock': '-n -r', 'add': ' --color=always -iGI8 -B1', 'complist': 'JVGREPOPTION.TXT' } : null;
 
 //マークなしのときの選択対象(")で括るように
 const markless = '"..\\%*name(C,%FD)"';
@@ -36,47 +37,49 @@ const argCmd = PPx.Arguments(0);
 const dogrep = (argCmd === 'grep') ? gr : (argCmd === 'rg') ? rg : jv;
 {
   const reload_opt = (() => { return ( arglen === 2) ? PPx.Arguments(1) : null; })();
-  const check_Mxxx = PPx.Extract('%*getcust(M_grepOptions)').split('\u000D\u000A');
+  const check_Mxxx = PPx.Extract('%*getcust(M_grepPPv)').split('\u000D\u000A');
   if (check_Mxxx.length === 3 || reload_opt === '1') {
     if (use_grep === true) {
-      PPx.Execute(`*setcust M_grepOptions:grep=*string i,cmd=grep %%: *string i,gopt=${gr.lock}${gr.add} %%: \
+      PPx.Execute(`*setcust M_grepPPv:grep=*string i,cmd=grep %%: *string i,gopt=${gr.lock}${gr.add} %%: \
       *string e,lock=${gr.lock} %%: *string e,add=${gr.add} %%: *string e,list=${gr.complist} %%: \
       *string e,flen=${gr.lock.length} %%: *string e,blen=${gr.lock.length + gr.add.length}`);
     } else {
-      PPx.Execute('*deletecust "M_grepOptions:grep"');
+      PPx.Execute('*deletecust "M_grepPPv:grep"');
     }
     if (use_ripgrep === true) {
-      PPx.Execute(`*setcust M_grepOptions:rg=*string i,cmd=rg %%: *string i,gopt=${rg.lock}${rg.add} %%: \
+      PPx.Execute(`*setcust M_grepPPv:rg=*string i,cmd=rg %%: *string i,gopt=${rg.lock}${rg.add} %%: \
       *string e,lock=${rg.lock} %%: *string e,add=${rg.add} %%: *string e,list=${rg.complist} %%: \
       *string e,flen=${rg.lock.length} %%: *string e,blen=${rg.lock.length + rg.add.length}`);
     } else {
-      PPx.Execute('*deletecust "M_grepOptions:rg"');
+      PPx.Execute('*deletecust "M_grepPPv:rg"');
     }
     if (use_jvgrep === true) {
-      PPx.Execute(`*setcust M_grepOptions:jvgrep=*string i,cmd=jvgrep %%: *string i,gopt=${jv.lock}${jv.add} %%: \
+      PPx.Execute(`*setcust M_grepPPv:jvgrep=*string i,cmd=jvgrep %%: *string i,gopt=${jv.lock}${jv.add} %%: \
       *string e,lock=${jv.lock} %%: *string e,add=${jv.add} %%: *string e,list=${jv.complist} %%: \
       *string e,flen=${jv.lock.length} %%: *string e,blen${jv.lock.length + jv.add.length}`);
     } else {
-      PPx.Execute('*deletecust "M_grepOptions:jvgrep"');
+      PPx.Execute('*deletecust "M_grepPPv:jvgrep"');
     }
   }
 }
+
 // optionボタンの設定
 PPx.Execute('*string i,Edit_OptionCmd=*string i,gopt=%%*input("%%se"lock"%%se"add"" \
   -title:"Option  ※%%se"lock"は外さないこと※" -mode:e -select:%%se"flen",%%se"blen" \
-  -k *completelist /set /file:"%%\'list\'\\%%se"list"") %%: *setcaption %%se"cmd" %%si"gopt" ※\\=\\\\\\\\');
+  -k *completelist /set /file:"%%\'list\'\\%%se"list"") %%: *setcaption %%si"cmd" %%si"gopt" ※\\=\\\\\\\\');
 
 // 検索文字の入力とエスケープ処理
 const str = (esc => {
   try {
     PPx.Execute(`*string i,gopt=${dogrep.lock}${dogrep.add}`);
-    return esc = PPx.Extract(`%*script(%'scr'%\\compCode.js,"iOs","""%%","${argCmd} %%si""gopt"" ※\\=\\\\\\\\"," *execute %%%%%%%%M_grepOptions,!${argCmd}")`);
+    return esc = PPx.Extract(`%*script(%'scr'%\\compCode.js,"iOs","""%%","${argCmd} %%si""gopt"" ※\\=\\\\\\\\", \
+      "*string e,px=PPv %%%%: *execute %%%%%%%%M_grepPPv,!${argCmd}")`);
   } catch (e) {
     PPx.Execute('*string i,gopt=');
     PPx.Echo(e);
     PPx.Quit(-1);
   } finally {
-    if (esc === '' ) {
+    if (esc === '') {
       PPx.Execute('*string i,gopt=');
       PPx.Quit(-1);
     }
---
+++ ppx/script/es6/grep.js
@@ -0,0 +1,180 @@
+﻿//!*script
+/* grep結果を出力 */
+//
+// PPx.Arguments() = (0)出力ファイル, (1)grep|rg|jvgrep:初期選択するgrepコマンド, (2)LF|PPv:初期選択の出力先 (3)1:オプションの再登録, 2:M_grepを削除して終了
+
+'use strict';
+
+const arglen = PPx.Arguments.length;
+
+if (arglen < 3) {
+  PPx.Echo('引数が足りません');
+  PPx.Quit(-1);
+}
+
+if (arglen === 4 && PPx.Arguments(3) === '2') {
+  PPx.Execute('*deletecust "M_grep"');
+  PPx.SetPopLineMessage('Delete > M_grep');
+  PPx.Quit(-1);
+}
+/////////* 初期設定 *////////////
+
+// 使用するコマンド
+const use_LF_grep    = true;
+const use_LF_rg      = true;
+const use_LF_jvgrep  = true;
+const use_PPv_grep   = true;
+const use_PPv_rg     = true;
+const use_PPv_jvgrep = true;
+
+// grep_option lockは固定。addの値を変更
+// ※addの記述はハイフン(-)で始まる場合スペースを１つ入れる
+// grep
+const grLF = (use_LF_grep === true)
+  ? { 'op': 'LF', 'lock': '-nH', 'add': 'irEC1 --color=never', 'complist': 'GREPOPTION.TXT' } : null;
+const grPPv = (use_PPv_grep === true)
+  ? { 'op': 'LF', 'lock': '-nH', 'add': 'irEC1 --color=always', 'complist': 'GREPOPTION.TXT' } : null;
+// ripgrep
+const rgLF = (use_LF_rg === true)
+  ? { 'op': 'LF', 'lock': '-n --no-heading', 'add': ' --color never -Li -C1', 'complist': 'RGOPTION.TXT' } : null;
+const rgPPv = (use_PPv_rg === true)
+  ? { 'op': 'ppv', 'lock': '-n --no-heading', 'add': ' --color always -Li -C1', 'complist': 'RGOPTION.TXT' } : null;
+// jvgrep
+const jvLF = (use_LF_jvgrep === true)
+  ? { 'op': 'ppv', 'lock': '-n -r', 'add': ' --color=never -iGI -B1', 'complist': 'JVGREPOPTION.TXT' } : null;
+const jvPPv = (use_PPv_jvgrep === true)
+  ? { 'op': 'ppv', 'lock': '-n -r', 'add': ' --color=always -iGI -B1', 'complist': 'JVGREPOPTION.TXT' } : null;
+
+//マークなしのときの選択対象(")で括るように
+const markless = '"..\\%*name(C,%FD)"';
+
+/////////////////////////////////
+
+const arg = { 'listfile': PPx.Arguments(0), 'cmd': PPx.Arguments(1), 'output': PPx.Arguments(2) };
+const ppxid = PPx.Extract('%n');
+const dogrep = (() => {
+  switch(arg.cmd + arg.output) {
+    case 'grepLF':    return grLF;
+    case 'grepPPv':   return grPPv;
+    case 'rgLF':      return rgLF;
+    case 'rgPPv':     return rgPPv;
+    case 'jvgrepLF':  return jvLF;
+    case 'jvgrepPPv': return jvPPv;
+  }
+})();
+
+{
+  const reload_opt = (arglen === 4) ? PPx.Arguments(3) : null;
+  const check_Mxxx = PPx.Extract('%*getcust(M_grep)').split('\u000D\u000A');
+  if (check_Mxxx.length === 3 || reload_opt === '1') {
+    if (use_LF_grep === true) {
+      PPx.Execute(`*setcust M_grep:LF_grep=*string i,cmd=grep %%: *string i,gopt=${grLF.lock}${grLF.add} %%: \
+      *string e,lock=${grLF.lock} %%: *string e,add=${grLF.add} %%: *string e,list=${grLF.complist} %%: \
+      *string e,flen=${grLF.lock.length} %%: *string e,blen=${grLF.lock.length + grLF.add.length} %%: \
+      *string i,output=LF`);
+    } else {
+      PPx.Execute('*deletecust "M_grep:LF_grep"');
+    }
+    if (use_PPv_grep === true) {
+      PPx.Execute(`*setcust M_grep:PPv_grep=*string i,cmd=grep %%: *string i,gopt=${grPPv.lock}${grPPv.add} %%: \
+      *string e,lock=${grPPv.lock} %%: *string e,add=${grPPv.add} %%: *string e,list=${grPPv.complist} %%: \
+      *string e,flen=${grPPv.lock.length} %%: *string e,blen=${grPPv.lock.length + grPPv.add.length} %%: \
+      *string i,output=PPv`);
+    } else {
+      PPx.Execute('*deletecust "M_grep:PPv_grep"');
+    }
+    if (use_LF_rg === true) {
+      PPx.Execute(`*setcust M_grep:LF_rg=*string i,cmd=rg %%: *string i,gopt=${rgLF.lock}${rgLF.add} %%: \
+      *string e,lock=${rgLF.lock} %%: *string e,add=${rgLF.add} %%: *string e,list=${rgLF.complist} %%: \
+      *string e,flen=${rgLF.lock.length} %%: *string e,blen=${rgLF.lock.length + rgLF.add.length} %%: \
+      *string i,output=LF`);
+    } else {
+      PPx.Execute('*deletecust "M_grep:LF_rg"');
+    }
+    if (use_PPv_rg === true) {
+      PPx.Execute(`*setcust M_grep:PPv_rg=*string i,cmd=rg %%: *string i,gopt=${rgPPv.lock}${rgPPv.add} %%: \
+      *string e,lock=${rgPPv.lock} %%: *string e,add=${rgPPv.add} %%: *string e,list=${rgPPv.complist} %%: \
+      *string e,flen=${rgPPv.lock.length} %%: *string e,blen=${rgPPv.lock.length + rgPPv.add.length} %%: \
+      *string i,output=PPv`);
+    } else {
+      PPx.Execute('*deletecust "M_grep:PPv_rg"');
+    }
+    if (use_LF_jvgrep === true) {
+      PPx.Execute(`*setcust M_grep:LF_jvgrep=*string i,cmd=jvgrep %%: *string i,gopt=${jvLF.lock}${jvLF.add} %%: \
+      *string e,lock=${jvLF.lock} %%: *string e,add=${jvLF.add} %%: *string e,list=${jvLF.complist} %%: \
+      *string e,flen=${jvLF.lock.length} %%: *string e,blen${jvLF.lock.length + jvLF.add.length} %%: \
+      *string i,output=LF`);
+    } else {
+      PPx.Execute('*deletecust "M_grep:LF_jvgrep"');
+    }
+    if (use_PPv_jvgrep === true) {
+      PPx.Execute(`*setcust M_grep:PPv_jvgrep=*string i,cmd=jvgrep %%: *string i,gopt=${jvPPv.lock}${jvPPv.add} %%: \
+      *string e,lock=${jvPPv.lock} %%: *string e,add=${jvPPv.add} %%: *string e,list=${jvPPv.complist} %%: \
+      *string e,flen=${jvPPv.lock.length} %%: *string e,blen${jvPPv.lock.length + jvPPv.add.length} %%: \
+      *string i,output=PPv`);
+    } else {
+      PPx.Execute('*deletecust "M_grep:PPv_jvgrep"');
+    }
+  }
+}
+
+// optionボタンの設定
+PPx.Execute('*string i,Edit_OptionCmd=*string i,gopt=%%*input("%%se"lock"%%se"add"" \
+  -title:"Option  ※%%se"lock"は外さないこと※" -mode:e -select:%%se"flen",%%se"blen" \
+  -k *completelist /set /file:"%%\'list\'\\%%se"list"") %%: *setcaption [%%si"output"] %%si"cmd" %%si"gopt" ※\\=\\\\\\\\');
+
+// 検索文字の入力とエスケープ処理
+const str = (esc => {
+  try {
+    PPx.Execute(`*string i,gopt=${dogrep.lock}${dogrep.add}`);
+    return esc = PPx.Extract(`%*script(%'scr'%\\compCode.js,"iOs","""%%","[${arg.output}] ${arg.cmd} %%si""gopt"" ※\\=\\\\\\\\", \
+      "*execute %%%%%%%%M_grep,!${arg.output}_${arg.cmd}")`);
+  } catch (e) {
+    PPx.Execute('*string i,gopt=');
+    PPx.Echo(e);
+    PPx.Quit(-1);
+  } finally {
+    if (esc === '') {
+      PPx.Execute('*string i,gopt=');
+      PPx.Quit(-1);
+    }
+  }
+})();
+
+const tPath = (PPx.EntryMarkCount) ? '%#FCB' : markless;
+
+// const pDir = PPx.Extract('%FD').replace(/\\/g, '\\\\\\\\');
+// const dirType = PPx.DirectoryType;
+
+// PPx.Execute(`*run -noppb -cmd -min -wait:later ${grep_cmd} %si"gopt" "${str}" ${tPath} | sed -r 's/^(.*)[:-]([0-9]*)([:-])(.*)/"\\1","\\2",A:H"\\3",C:0.0,L:0.0,W:0.0,S:0.0,M:0,T:"\\4"/' | awk 'BEGIN {print ";ListFile\\r\\n;Base=${pDir}|${dirType}\\r\\n\\"file\\",\\"line\\",A:H5,C:0.0,L:0.0,W:0.0,S:0.0,M:0,T:\\"result: ${str}\\""} {print $0}' | %Os nkf -w16L > ${arg.listfile} %: *wait -run`);
+
+// grepの結果をutf16lbで出力
+if (PPx.Extract('%si"output"') === 'PPv') {
+  PPx.Execute(`*run -noppb -min %si"cmd" %si"gopt" "${str}" ${tPath} | %0ppvw -bootid:w -esc -document -k *string p,grep=1 %%: *find "${str}"`);
+} else {
+  PPx.Execute(`%Obn %si"cmd" %si"gopt" "${str}" ${tPath} | %Os nkf -w16B > ${arg.listfile}`);
+
+  // リストの整形
+  const fso = PPx.CreateObject('Scripting.FileSystemObject');
+  const pDir = PPx.Extract('%FD');
+  const dirType = PPx.DirectoryType;
+  const result = [`;ListFile\u000D\u000A;Base=${pDir}|${dirType}\u000D\u000A"file","line",A:H5,C:0.0,L:0.0,W:0.0,S:0.0,H:0,M:0,T:"result => ${str}"`];
+  let fsoTlist = fso.OpenTextFile(arg.listfile, 1, false, -1);
+
+  while (!fsoTlist.AtEndOfStream) {
+    fsoTlist.ReadLine().replace(/^([^-:]*)[-:](\d*)([-:])\s*(.*)/, (match, p1, p2, p3, p4) => {
+      p1 = (p1 == '') ? p3 : p1.replace(/^\.\.\\.*\//, '');
+      p3 = (p3.indexOf(':') != -1) ? 0 : 3;
+      p4 = p4.replace(/"/g, '""');
+      result.push(`"${p1}","${p2}",A:H${p3},C:0.0,L:0.0,W:0.0,S:0.0,H:0,M:0,T:"${p4}"`);
+    });
+  }
+
+  // 置換結果を書き出して上書き
+  fsoTlist = fso.OpenTextFile(arg.listfile, 2, true, -1);
+  fsoTlist.Write(result.join('\u000D\u000A'));
+  fsoTlist.Close();
+}
+
+PPx.Execute(`*execute ${ppxid},*string i,cmd= %%: *string i,gopt=`);
+

b09178e650817d4c03e8b80719fbe977cd9249d8
tar80 <tar80master@gmail.com>
2021-05-11 01:13:38 +0900

    Refactor

--- ppx/script/es6/selStr.js
+++ ppx/script/es6/selStr.js
@@ -2,7 +2,9 @@
 /* 一行編集上で編集中の文字の選択状態を操作する */
 //
 // PPx.Arguments(0) = "(lparam(wparam))"
-// 引数は正規表現で指定する
+// 引数は正規表現で指定
+// lparam:編集中テキストの全体を指定
+// wparam:選択部分を指定
 // PPXMES.DLLが必要
 // 参照元:https://egg.5ch.net/test/read.cgi/software/1476708638/409
 
